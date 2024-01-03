927ab38caabd78017bd42acbc1f5b5c5d46b52e8
shashank.s <148774696+shashankssvi@users.noreply.github.com>
2024-01-03 06:06:45 +0530

    Update views.py

--- holyday/holyday1/views.py
+++ holyday/holyday1/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 import csv
-def printholiday():
-    f1=open("holiday/holiday1/Holidays_2024.csv","r")
+from holiday.settings import BASE_DIR
+import os
+from holiday1.forms import inputform
+def getholiday():
+    f1=open(os.path.join(BASE_DIR,"holiday1/Holidays_2024.csv"),"r")
     list1=[]
     date1=[]
     day1=[]
     name1=[]
     state1=[]
+    holidays=[]
     info1=csv.reader(f1)
     for line in info1:
         list1.append(line)
@@ -14,13 +18,15 @@ def printholiday():
         day1.append(line[1])
         name1.append(line[2])
         state1.append(line[3].replace("\n",""))
-    #print(state1)
+    
     for i,s in enumerate(state1):
         if "KA" in s :
-            print(date1[i],name1[i]," is a holiday")
-#Program not working for national except
-printholiday()
-    
+            holidays.append([date1[i],day1[i],name1[i]])
+    return holidays
+
 def holiday(request):
-    
-    return render(request,"holiday1/index.html")
+    if request.method=="POST":
+        form=inputform(request.POST)
+    list1=getholiday()
+    print(list1)
+    return render(request,"holiday1/index.html",{"param1":list1})
