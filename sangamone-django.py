f449c44eb84cf971d63df612de198b0120b99374
shashankssvi <personal.shashank.s@gmail.com>
2024-04-20 06:35:43 +0530

    Django

--- sangamone-django.py
+++ sangamone-django.py
@@ -5,16 +5,22 @@ os.chdir(dproject)
 os.chdir(f"./{dproject}")
 f1=open("settings.py","r+")
 info1=f1.read()
-list1=[]
-a=info1.replace("'django.contrib.staticfiles',",f"'django.contrib.staticfiles',\n\t'{dapp}',")
-f1.seek(0)
-f1.write(a)
+if f"{dapp}'," not in info1:
+    a=info1.replace("'django.contrib.staticfiles',",f"'django.contrib.staticfiles',\n\t'{dapp}',")
+    f1.seek(0)
+    f1.write(a)
 f1.close()
 
 f1=open("urls.py","r+")
 info1=f1.read()
-any1=info1.replace("path('admin/', admin.site.urls),",f"""path('admin/', admin.site.urls),\n\turlpatterns = [path("{dapp}" ,include("{dapp}.urls")),]""")
-f1.write(any1)
+if f"{dapp}.urls" not in info1 and "from django.urls import path,include" not in info1:
+    any1=info1.replace("path('admin/', admin.site.urls),",f"""path('admin/', admin.site.urls),\n\tpath("{dapp}/" ,include("{dapp}.urls")),""").replace("from django.urls import path","from django.urls import path,include")
+    f1.seek(0)
+    f1.write(any1)
+else:
+    any1=info1.replace("path('admin/', admin.site.urls),",f"""path('admin/', admin.site.urls),\n\tpath("{dapp}/" ,include("{dapp}.urls")),""")
+    f1.seek(0)
+    f1.write(any1)
 f1.close()
 
 os.chdir(f"../{dapp}")
@@ -25,10 +31,23 @@ def home(request):
 f1.write(any)
 f1.close()
 
-f1=open("urls.py","w")
-f1.write(f"""from django.urls import path
-from {dapp}.views import home
-urlpatterns = [path('/{dapp}', home),]""")
+open('urls.py','w')
+f1=open("urls.py","r+")
+if "urlpatterns = [" not in f1.read():
+    f1.write(f"from django.urls import path\nfrom {dapp}.views import home\nurlpatterns = [\n\tpath('', home),]")
+else:
+    a=f1.read().replace("urlpatterns = [",f"urlpatterns = [\n\tpath('', home),").replace("from django.urls import path",f"from django.urls import path\nfrom {dapp}.views import home")
+    f1.seek(0)
+    f1.write(a)
+# else:
+#     for i in range(1,100,1):
+#         if f"/{i}" in f1.read():
+#             continue
+#         else:
+#             a=f1.read().replace("urlpatterns = [",f"urlpatterns = [\n\tpath('{i}', home),").replace("from django.urls import path",f"from django.urls import path\nfrom {dapp}.views import home")
+#             f1.seek(0)
+#             f1.write(a)
+#             break
 f1.close()
 
 
