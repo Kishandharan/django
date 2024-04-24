ca63591f70b97bc1604640bcf7c2a8444053c4b9
shashankssvi <personal.shashank.s@gmail.com>
2024-04-24 18:11:10 +0530

    Django

--- sangamone-django1.0.py
+++ sangamone-django1.0.py
@@ -1,8 +1,8 @@
 import os
 dproject=input("Enter the django project name: ")
 dapp=input("Enter the django app name: ")
-os.chdir(dproject)             # Changing path to django folder
-os.chdir(f"./{dproject}")      # Entering inside the project file
+os.chdir(dproject)
+os.chdir(f"./{dproject}")
 f1=open("settings.py","r+")
 info1=f1.read()
 if f"{dapp}'," not in info1:
@@ -23,40 +23,52 @@ else:
     f1.write(any1)
 f1.close()
 
-os.chdir(f"../{dapp}")        # Navigationg to app folder
-f1=open("views.py","w")       
-any="""from django.shortcuts import render
-def home(request):
-    return render(request,'"""+dapp+"""/index.html',{'param1':"hello world"})"""
-f1.write(any)
-f1.close()
+try:
+    os.chdir(f"../{dapp}")
+    f1=open("views.py","w")
+    any="""from django.shortcuts import render
+    def home(request):
+        return render(request,'"""+dapp+"""/index.html',{'param1':"hello world"})"""
+    f1.write(any)
+    f1.close()
 
-open('urls.py','w')          # Creating urls.py inside the app folder
-f1=open("urls.py","r+")
-if "urlpatterns = [" not in f1.read():
-    f1.write(f"from django.urls import path\nfrom {dapp}.views import home\nurlpatterns = [\n\tpath('', home),]")
-else:
-    a=f1.read().replace("urlpatterns = [",f"urlpatterns = [\n\tpath('', home),").replace("from django.urls import path",f"from django.urls import path\nfrom {dapp}.views import home")
-    f1.seek(0)
-    f1.write(a)
-f1.close()
+    open('urls.py','w')
+    f1=open("urls.py","r+")
+    if "urlpatterns = [" not in f1.read():
+        f1.write(f"from django.urls import path\nfrom {dapp}.views import home\nurlpatterns = [\n\tpath('', home),]")
+    else:
+        a=f1.read().replace("urlpatterns = [",f"urlpatterns = [\n\tpath('', home),").replace("from django.urls import path",f"from django.urls import path\nfrom {dapp}.views import home")
+        f1.seek(0)
+        f1.write(a)
+    # else:
+    #     for i in range(1,100,1):
+    #         if f"/{i}" in f1.read():
+    #             continue
+    #         else:
+    #             a=f1.read().replace("urlpatterns = [",f"urlpatterns = [\n\tpath('{i}', home),").replace("from django.urls import path",f"from django.urls import path\nfrom {dapp}.views import home")
+    #             f1.seek(0)
+    #             f1.write(a)
+    #             break
+    f1.close()
 
 
-os.makedirs(f"templates/{dapp}")     # Creating templates folder for index.py 
-os.chdir(f"templates/{dapp}")
-f1=open("index.html","w")
-f1.write("""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>Hello World</p>
-    <p>{{param1}}</p>
-</body>
-</html>
-""")
-f1.close()
-
+    os.makedirs(f"templates/{dapp}")
+    os.chdir(f"templates/{dapp}")
+    f1=open("index.html","w")
+    f1.write("""<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <p>Hello World</p>
+        <p>{{param1}}</p>
+    </body>
+    </html>
+    """)
+    f1.close()
+except FileNotFoundError:
+    print("App not created or check wether the app is created inside the project")
+    print(f"run command 'django-admin startapp {dapp}'")
